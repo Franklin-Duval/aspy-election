9dc87886dae9411bdcbaeaaddde93d7afbac51c2
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-16 18:38:21 +0100

    feat: create Voter entity

---
+++ server/modules/voter/entities/voter.entity.ts
@@ -0,0 +1,43 @@
+import { DepartmentType, LevelType } from 'server/shared/customTypes';
+
+interface VoterEntityJSON {
+  _id: string;
+  name: string;
+  surename: string;
+  contact: string;
+  email: string;
+  matricule: string;
+  department: DepartmentType;
+  level: LevelType;
+  image: string;
+  creation_date: string;
+  voted: boolean;
+}
+
+export class VoterEntity implements VoterEntityJSON {
+  _id: string;
+  name: string;
+  surename: string;
+  contact: string;
+  email: string;
+  matricule: string;
+  department: DepartmentType;
+  level: LevelType;
+  image: string;
+  creation_date: string;
+  voted: boolean;
+
+  constructor(voter: VoterEntityJSON) {
+    this._id = voter._id;
+    this.name = voter.name;
+    this.surename = voter.surename;
+    this.contact = voter.contact;
+    this.email = voter.email;
+    this.matricule = voter.matricule;
+    this.department = voter.department;
+    this.level = voter.level;
+    this.image = voter.image;
+    this.creation_date = voter.creation_date;
+    this.voted = voter.voted;
+  }
+}
