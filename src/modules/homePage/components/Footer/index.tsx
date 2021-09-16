c815151d887ea055b5359c29b707274ae811ab61
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-16 20:23:07 +0100

    feat: update Footer component

--- src/modules/homePage/components/Footer/index.tsx
+++ src/modules/homePage/components/Footer/index.tsx
@@ -1,37 +1,16 @@
 import styled from '@emotion/styled';
 import { Space } from 'antd';
 import React from 'react';
-import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa';
+import { FaChrome, FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 const FooterContainer = styled.div`
-  min-height: 300px;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   padding: 20px;
-  background-color: #48347c;
+  background-color: #263238;
   color: white;
-
-  > .row {
-    display: flex;
-    justify-content: space-around;
-    flex-wrap: wrap;
-
-    h3 {
-      color: white;
-      font-size: 20px;
-    }
-
-    > div {
-      min-width: 250px;
-      margin: 5px;
-      > p {
-        font-size: 16px;
-        color: rgba(255, 255, 255, 0.8);
-        line-height: 0.8;
-      }
-    }
-  }
 
   .social-icon {
     display: flex;
@@ -46,56 +25,39 @@ const FooterContainer = styled.div`
 
 const socialLinks = [
   {
-    link: 'https://www.facebook.com/uzenze',
+    link: 'https://www.facebook.com/ASPYonline',
     icon: <FaFacebook size={25} />,
   },
   {
-    link: 'https://www.instagram.com/uzenze_group/?hl=fr',
+    link: 'https://www.instagram.com/aspy_2000/',
     icon: <FaInstagram size={25} />,
   },
   {
-    link: 'https://www.linkedin.com/company/uzenze/',
+    link: 'https://www.linkedin.com/company/aspy-official/',
     icon: <FaLinkedin size={25} />,
   },
   {
-    link: 'https://www.youtube.com/channel/UCdbcCa8Z3_AU6rCTmG86llA',
-    icon: <FaYoutube size={25} />,
+    link: 'https://www.aspyonline.org/',
+    icon: <FaChrome size={25} />,
+  },
+  {
+    link: 'officialaspy@gmail.com',
+    icon: <MdEmail size={25} />,
   },
 ];
 
 export const Footer = () => {
   return (
     <FooterContainer id='contact'>
-      <div className='row'>
-        <div>
-          <h3>MAEL par Uzenze</h3>
-          <p>Mael logo</p>
-        </div>
-        <div>
-          <h3>Services</h3>
-          <p>Télécharger version desktop</p>
-          <p>Comment ça marche</p>
-        </div>
-        <div>
-          <h3>Contact</h3>
-          <p>Adresse: Chateau NGOA EKELE,Yaoundé</p>
-          <p>Téléphone: (+237) 677 40 40 40</p>
-          <p>Adresse Mail: contact@uzenze.com</p>
-        </div>
-      </div>
-
-      <div>
-        <hr />
-        <div className='social-icon'>
-          <Space>
-            {socialLinks.map(({ link, icon }) => (
-              <a key={link} href={link} target='_blank' rel='noreferrer'>
-                {icon}
-              </a>
-            ))}
-          </Space>
-          <p>Copyright © 2021 - Uzenze.</p>
-        </div>
+      <div className='social-icon'>
+        <Space size={30}>
+          {socialLinks.map(({ link, icon }) => (
+            <a key={link} href={link} target='_blank' rel='noreferrer'>
+              {icon}
+            </a>
+          ))}
+        </Space>
+        <p>Copyright © 2021 - ASPY.</p>
       </div>
     </FooterContainer>
   );
