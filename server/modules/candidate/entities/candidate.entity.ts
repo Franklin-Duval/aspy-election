b9876f18275e1e1cbdad6a5a81ec0b84110e2ab5
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-16 19:21:02 +0100

    feat: create candidate entity

---
+++ server/modules/candidate/entities/candidate.entity.ts
@@ -0,0 +1,41 @@
+import {
+  VoterEntity,
+  VoterEntityJSON,
+} from 'server/modules/voter/entities/voter.entity';
+
+interface CandidateEntityJSON extends VoterEntityJSON {
+  planOfAction: string;
+  manifesto: string;
+  post: string;
+  numberVotes: number;
+}
+
+export class CandidateEntity
+  extends VoterEntity
+  implements CandidateEntityJSON
+{
+  planOfAction: string;
+  manifesto: string;
+  post: string;
+  numberVotes: number;
+  constructor(candidate: CandidateEntityJSON) {
+    super({
+      _id: candidate._id,
+      name: candidate.name,
+      surename: candidate.surename,
+      contact: candidate.contact,
+      email: candidate.email,
+      matricule: candidate.matricule,
+      department: candidate.department,
+      level: candidate.level,
+      image: candidate.image,
+      creation_date: candidate.creation_date,
+      voted: candidate.voted,
+    });
+
+    this.planOfAction = candidate.planOfAction;
+    this.manifesto = candidate.manifesto;
+    this.post = candidate.post;
+    this.numberVotes = candidate.numberVotes;
+  }
+}
