8b2e336e3b732c14fd6c692044c9b0ef6a88c119
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-16 20:51:36 +0100

    feat: update color of Button

--- src/modules/homePage/styles/Button.ts
+++ src/modules/homePage/styles/Button.ts
@@ -9,17 +9,17 @@ export const Button = styled.button`
   padding: 10px;
   cursor: pointer;
   border-radius: 5px;
-  background-color: #7a39e0;
+  background-color: #263238;
   border-color: transparent;
   &:hover {
     background-color: white;
-    color: #7a39e0;
-    border: 2px solid #7a39e0;
+    color: #263238;
+    border: 2px solid #263238;
   }
 `;
 
 export const ButtonOutline = styled.button`
-  color: #7a39e0;
+  color: #263238;
   font-size: 16px;
   height: 50px;
   font-family: 'Montserrat';
@@ -28,9 +28,9 @@ export const ButtonOutline = styled.button`
   cursor: pointer;
   border-radius: 5px;
   background-color: white;
-  border: 2px solid #7a39e0;
+  border: 2px solid #263238;
   &:hover {
-    background-color: #7a39e0;
+    background-color: #263238;
     color: white;
   }
 `;
