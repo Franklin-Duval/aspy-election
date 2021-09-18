4499cb901f7b5a79a260e04f0d1b6c19dc9a2383
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-18 09:37:37 +0100

    feat: create DataTable component

---
+++ src/modules/shared/DataTable/index.tsx
@@ -0,0 +1,76 @@
+import styled from '@emotion/styled';
+import { Input, Space, Table } from 'antd';
+import { ColumnsType } from 'antd/lib/table';
+import { useState } from 'react';
+
+const DataTableContainer = styled.div`
+  zoom: 0.9;
+
+  th {
+    background-color: #263238 !important;
+    color: white !important;
+    font-weight: bold;
+  }
+
+  .odd-row {
+    background-color: #e0e0e0;
+  }
+
+  .even-row {
+    background-color: #fafafa;
+  }
+
+  @media (min-width: 768px) {
+    zoom: 1;
+  }
+`;
+
+export const DataTable = <T extends { _id: string }>({
+  data,
+  columns,
+  filterFunction,
+  onItemClick,
+  AddButton = <span>Add a button please</span>,
+}: {
+  columns: ColumnsType<T>;
+  data: T[];
+  loading?: boolean;
+  filterFunction?: (dataItem: T, filterValue: string) => boolean;
+  onItemClick?: (item: T) => void;
+  AddButton?: React.ReactElement;
+}) => {
+  const [filterValue, setFilterValue] = useState('');
+  const [rowId, setRowId] = useState('');
+
+  const dataToShow = data?.filter((dataItem) =>
+    filterFunction?.(dataItem, filterValue),
+  );
+  return (
+    <DataTableContainer>
+      <Space style={{ marginBottom: 10 }}>
+        <Input
+          placeholder='Enter a name to filter'
+          style={{ maxWidth: 200 }}
+          value={filterValue}
+          onChange={(e) => setFilterValue(e.target.value)}
+        />
+        {AddButton}
+      </Space>
+      <Table
+        dataSource={dataToShow}
+        columns={columns}
+        size='small'
+        rowKey='_id'
+        rowClassName={(row, index) =>
+          rowId == row._id ? 'is-bold' : index % 2 == 0 ? 'even-row' : 'odd-row'
+        }
+        onRow={(row) => ({
+          onClick: () => {
+            setRowId(row._id);
+            onItemClick?.(row);
+          },
+        })}
+      ></Table>
+    </DataTableContainer>
+  );
+};
