c51100561940d4f006983b5e8f787657f1d6369c
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-18 07:14:56 +0100

    feat: update candidate list route in ROUTES

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ export const ROUTES = {
   HOME_PAGE: '/',
   VOTER: {
     REGISTRATION: '/voter/registration',
-    CANDIDATE_LIST: '/voter/candidate-list',
+    CANDIDATE_LIST: '/voter/candidateList',
   },
   CANDIDATE: {
     REGISTRATION: '/candidate/registration',
