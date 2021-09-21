0b9f04273584d550ce8879d72068990afa162d56
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-21 17:13:36 +0100

    feat: create pages/api/candidate/addLike.ts and pages/api/candidate/addDislike.ts routes

---
+++ pages/api/candidate/addLike.ts
@@ -0,0 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { candidateService } from 'server/modules/candidate/services/candidate.service';
+
+async function likesHandler(req: NextApiRequest, res: NextApiResponse) {
+  const { candidateId, voterId } = req.body;
+  const result = await candidateService.addLike(candidateId, voterId);
+  res.json(result);
+}
+
+export default likesHandler;
