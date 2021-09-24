a2aa51cdfdfb0005606061250522b8cae777fca2
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-24 11:49:41 +0100

    feat: update AuthGuard and use in Layout component

--- src/modules/shared/AuthGuard/index.tsx
+++ src/modules/shared/AuthGuard/index.tsx
@@ -1,11 +1,9 @@
 import { useSession } from 'next-auth/client';
-import { useRouter } from 'next/router';
 import React from 'react';
 import { Unauthorized } from '../Unathorized';
 
 export const AuthGuard: React.FC<{}> = ({ children }) => {
   const [session, loading] = useSession();
-  const router = useRouter();
 
   if (loading) return null;
 
