33fa9d0a3c055ec425c967c6f41041f987cb1eb7
Talom_Franklin <franklinfrost14@gmail.com>
2021-09-24 16:24:15 +0100

    feat: update addMultipleVote in votes service

--- server/modules/votes/services/votes.service.ts
+++ server/modules/votes/services/votes.service.ts
@@ -1,3 +1,5 @@
+import { candidateService } from 'server/modules/candidate/services/candidate.service';
+import { voterService } from 'server/modules/voter/services/voter.service';
 import { VoteEntity } from '../entities/votes.entity';
 import { votesDbService } from './votesdb.service';
 
@@ -8,8 +10,15 @@ class VotesService {
   addVote = async (vote: VoteEntity) => {
     return await votesDbService.addVote(vote);
   };
-  addMultipleVote = async (vote: VoteEntity[]) => {
-    return await votesDbService.addMultipleVote(vote);
+  addMultipleVote = async (votes: VoteEntity[]) => {
+    const resultVotes = await votesDbService.addMultipleVote(votes);
+    for (let vote of votes) {
+      if (vote.candidate !== '----') {
+        await candidateService.addNumberVotes(vote.candidate);
+      }
+    }
+    await voterService.finishVote(votes[0].voter);
+    return resultVotes;
   };
 }
 
