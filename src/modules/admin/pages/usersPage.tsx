61da52f50f6de3b614617fe57760f1758d466ea1
Talom_Franklin <franklinfrost14@gmail.com>
2021-10-04 15:52:45 +0100

    feat: add stats in UserPage

--- src/modules/admin/pages/usersPage.tsx
+++ src/modules/admin/pages/usersPage.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { FaAngleRight } from 'react-icons/fa';
 import { VoterEntity } from 'server/modules/voter/entities/voter.entity';
 import { fetchCandidates } from 'src/modules/candidate/network/candidate.network';
 import { Layout } from 'src/modules/shared/Layout';
@@ -25,6 +26,44 @@ export const UsersPage = () => {
   return (
     <Layout>
       <h2>List of all registered users</h2>
+      <div>
+        <h3>
+          <FaAngleRight
+            color='red'
+            style={{ marginBottom: -3, marginRight: 5 }}
+          />
+          N° Total users: {users.length}{' '}
+        </h3>
+        <h3>
+          <FaAngleRight
+            color='red'
+            style={{ marginBottom: -3, marginRight: 5 }}
+          />
+          N° of users who voted:{' '}
+          {users.filter((user) => user.voted == true).length}
+        </h3>
+        <h3>
+          <FaAngleRight
+            color='red'
+            style={{ marginBottom: -3, marginRight: 5 }}
+          />
+          N° of users who have not voted:{' '}
+          {users.filter((user) => user.voted == false).length}
+        </h3>
+        <h3>
+          <FaAngleRight
+            color='red'
+            style={{ marginBottom: -3, marginRight: 5 }}
+          />
+          Percentage of participation:{' '}
+          {Math.round(
+            ((users.filter((user) => user.voted == true).length * 100) /
+              users.length) *
+              100,
+          ) / 100}{' '}
+          %
+        </h3>
+      </div>
       <div style={{ display: 'flex', flexWrap: 'wrap' }}>
         {users.map((user) => (
           <UserCard key={user._id} user={user} />
