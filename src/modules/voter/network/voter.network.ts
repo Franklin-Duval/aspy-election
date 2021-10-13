59eb9328499fc7d53ce9c26721e96b31d67ae484
Talom_Franklin <franklinfrost14@gmail.com>
2021-10-13 04:34:05 +0100

    feat: add ErrorType type in addVoter in voter network

--- src/modules/voter/network/voter.network.ts
+++ src/modules/voter/network/voter.network.ts
@@ -1,5 +1,6 @@
 import { VoterEntity } from 'server/modules/voter/entities/voter.entity';
 import { VoteEntity } from 'server/modules/votes/entities/votes.entity';
+import { ErrorType } from 'server/shared/customTypes';
 import { API_ROUTES } from 'src/modules/shared/ApiRoutes/API_ROUTES';
 import { customFetch, responseHandler } from 'src/shared/customFetch';
 
@@ -7,7 +8,9 @@ export const fetchVoters = (): Promise<VoterEntity[]> => {
   return customFetch.get(API_ROUTES.VOTERS.GET_ALL);
 };
 
-export const addVoter = (voterToCreate: VoterEntity): Promise<VoterEntity> => {
+export const addVoter = (
+  voterToCreate: VoterEntity,
+): Promise<VoterEntity | ErrorType> => {
   let formData = new FormData();
   formData.append('name', voterToCreate.name);
   formData.append('surename', voterToCreate.surename);
