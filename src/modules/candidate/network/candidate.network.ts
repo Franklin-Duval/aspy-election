c56e9d5c3cd99b95c34431a0fff6ac64a95ac091
Talom_Franklin <franklinfrost14@gmail.com>
2021-10-13 13:27:00 +0100

    fix: remove types in candidate and voter network

--- src/modules/candidate/network/candidate.network.ts
+++ src/modules/candidate/network/candidate.network.ts
@@ -1,5 +1,5 @@
 import { CandidateEntity } from 'server/modules/candidate/entities/candidate.entity';
-import { Application, ErrorType } from 'server/shared/customTypes';
+import { Application } from 'server/shared/customTypes';
 import { API_ROUTES } from 'src/modules/shared/ApiRoutes/API_ROUTES';
 import { customFetch, responseHandler } from 'src/shared/customFetch';
 
@@ -7,9 +7,7 @@ export const fetchCandidates = (): Promise<CandidateEntity[]> => {
   return customFetch.get(API_ROUTES.CANDIDATES.GET_ALL);
 };
 
-export const addCandidate = (
-  candidateToCreate: CandidateEntity,
-): Promise<CandidateEntity | ErrorType> => {
+export const addCandidate = (candidateToCreate: CandidateEntity) => {
   let formData = new FormData();
   formData.append('name', candidateToCreate.name);
   formData.append('surename', candidateToCreate.surename);
--- src/modules/voter/network/voter.network.ts
+++ src/modules/voter/network/voter.network.ts
@@ -1,6 +1,5 @@
 import { VoterEntity } from 'server/modules/voter/entities/voter.entity';
 import { VoteEntity } from 'server/modules/votes/entities/votes.entity';
-import { ErrorType } from 'server/shared/customTypes';
 import { API_ROUTES } from 'src/modules/shared/ApiRoutes/API_ROUTES';
 import { customFetch, responseHandler } from 'src/shared/customFetch';
 
@@ -8,9 +7,7 @@ export const fetchVoters = (): Promise<VoterEntity[]> => {
   return customFetch.get(API_ROUTES.VOTERS.GET_ALL);
 };
 
-export const addVoter = (
-  voterToCreate: VoterEntity,
-): Promise<VoterEntity | ErrorType> => {
+export const addVoter = (voterToCreate: VoterEntity) => {
   let formData = new FormData();
   formData.append('name', voterToCreate.name);
   formData.append('surename', voterToCreate.surename);
